c9c65e71aeaefe304ff949712cd8fc4165f21b35
mrazomej <manuel.razo.m@gmail.com>
2018-10-29 08:19:29 -0700

    update plotting style

--- src/plotting_style.py
+++ src/plotting_style.py
@@ -5,31 +5,33 @@ import matplotlib.cm as cm
 # Seaborn, useful for graphics
 import seaborn as sns
 
+
 def set_plotting_style():
     """
     Formats plotting enviroment to that used in Physical Biology of the Cell,
     2nd edition. To format all plots within a script, simply execute
     `mwc_induction_utils.set_plotting_style() in the preamble.
     """
-    rc = {'lines.linewidth': 2,
-          'axes.labelsize': 16,
-          'axes.titlesize': 18,
+    rc = {'lines.linewidth': 1.25,
+          'axes.labelsize': 8,
+          'axes.titlesize': 9,
           'axes.facecolor': '#E3DCD0',
-#          'xtick.major' : 20,
-          'xtick.labelsize': 13,
-          'ytick.labelsize': 13,
+          'xtick.labelsize': 7,
+          'ytick.labelsize': 7,
           'font.family': 'Lucida Sans Unicode',
-          'grid.linestyle': ':',
-          'grid.linewidth': 1.5,
+          'grid.linestyle': '-',
+          'grid.linewidth': 0.5,
           'grid.color': '#ffffff',
-          'mathtext.fontset': 'stixsans',
-          'mathtext.sf': 'sans',
-          'legend.frameon': True,
-          'legend.fontsize': 11}
+          'legend.fontsize': 8}
     plt.rc('text.latex', preamble=r'\usepackage{sfmath}')
-    plt.rc('mathtext', fontset='stixsans', sf='sans')
+    plt.rc('xtick.major', pad=-1)
+    plt.rc('ytick.major', pad=-1)
+    plt.rc('mathtext', fontset='stixsans', sf='sansserif')
+    plt.rc('figure', figsize=[4.0, 3.0])
+    plt.rc('svg', fonttype='none')
+    plt.rc('legend', title_fontsize='8', frameon=True, 
+           facecolor='#E3DCD0', framealpha=1)
     sns.set_style('darkgrid', rc=rc)
     sns.set_palette("colorblind", color_codes=True)
     sns.set_context('notebook', rc=rc)
-
 
